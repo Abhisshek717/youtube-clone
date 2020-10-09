532912a6d1edcfa8827030d2d503f312c56ea95a
AbhisshekCP <saiabhisshek2000@gmail.com>
2020-10-09 23:22:29 +0530

    Updates

---
+++ precache-manifest.c8fd817ebf9677bf5d992f9b3ca08e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a6166eba5b369693f89840b22cfe64",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "9acf7d2bf372ed68bd09",
+    "url": "/youtube-clone/static/css/main.b71adff4.chunk.css"
+  },
+  {
+    "revision": "25ba7ebb558fd488aa07",
+    "url": "/youtube-clone/static/js/2.e4e12a9a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone/static/js/2.e4e12a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9acf7d2bf372ed68bd09",
+    "url": "/youtube-clone/static/js/main.7f4b5862.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  }
+]);
